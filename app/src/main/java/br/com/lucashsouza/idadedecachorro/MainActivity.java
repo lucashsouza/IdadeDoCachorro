737d87ebf5633ea5f924199597d05e13cb1f0a9b
Lucas Souza <lhenriquesouza00@gmail.com>
2021-05-10 22:04:15 -0300

    versao inicial

---
+++ app/src/main/java/br/com/lucashsouza/idadedecachorro/MainActivity.java
@@ -0,0 +1,29 @@
+  package br.com.lucashsouza.idadedecachorro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+  public class MainActivity extends AppCompatActivity {
+
+    private EditText editIdade;
+    private TextView textResultado;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editIdade = findViewById(R.id.editIdade);
+        textResultado = findViewById(R.id.textResultado);
+    }
+
+    public void calcularIdade(View view){
+        int idade = Integer.parseInt(editIdade.getText().toString());
+        int resultado = idade * 7;
+        textResultado.setText("Seu cachorro tem: " + resultado + " anos humanos");
+    }
+}
